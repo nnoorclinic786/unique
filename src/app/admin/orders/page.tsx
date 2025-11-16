2a3faf0c98c2b8cdd47e0a4408e8d0d43e98e6ce
MOHD MONISH <nnoorclinic786@gmail.com>
2025-11-16 13:21:08 +0000

    Build Error
    Parsing ecmascript source code failed
    
    ./src/app/admin/ord

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -179,7 +179,7 @@ export default function AdminOrdersPage() {
     ];
 
     const csvString = csvRows.join('\n');
-    const blob = new Blob([csvString], { type: 'text/csv;charset=utf-t-8;' });
+    const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
     const url = URL.createObjectURL(blob);
     link.setAttribute('href', url);
