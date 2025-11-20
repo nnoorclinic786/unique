1512c12367fe33541529d1829833732b5b864657
MOHD MONISH <nnoorclinic786@gmail.com>
2025-11-20 15:22:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7107721120-788a9",
+  "appId": "1:1075282149720:web:867d08f6e26141dc06652d",
+  "apiKey": "AIzaSyBsQxpeK7GN2EqnCGZvkDFgvDeSp0dRvD8",
+  "authDomain": "studio-7107721120-788a9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1075282149720"
+};
